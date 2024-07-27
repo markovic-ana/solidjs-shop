f16724f819699b5f9596950b72126e965070b802
Ana Markovic <markovicana@icloud.com>
2024-07-27 19:15:12 +0200

    children prop in card

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,30 @@ const App: Component = () => {
   return (
     <div class="container m-auto">
       <header>
+        <span class="material-symbols-outlined cursor-pointer">light_mode</span>
         <h1>Ninja Merch</h1>
       </header>
 
       <img class="rounded-md" src={banner} alt="site banner" />
 
       <div class="grid grid-cols-4 gap-10 my-4">
-        <Card />
-        <Card />
-        <Card />
+        <Card rounded>
+          <h2>1</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            nam voluptas dicta ad fuga. Id!
+          </p>
+          <button class="btn">view</button>{" "}
+        </Card>
+        <Card rounded>
+          <h2>2</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            nam voluptas dicta ad fuga. Id!
+          </p>
+          <button class="btn">view</button>
+          <p>Only $10</p>
+        </Card>
       </div>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,20 @@
-import { Component } from "solid-js";
+import { Component, JSXElement, children } from "solid-js";
 
-export const Card: Component<{}> = (props) => {
+interface ICardProps {
+  children: JSXElement;
+  flat?: boolean;
+  rounded?: boolean;
+}
+
+export const Card: Component<ICardProps> = (props) => {
+  const safeChildren = children(() => props.children);
+
   return (
-    <div class="bg-white p-4 text-center rounded-md shadow-md">
-      <h2>Card</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nam
-        voluptas dicta ad fuga. Id!
-      </p>
-      <button class="btn">Click me!</button>
+    <div
+      class="bg-white p-4 text-center"
+      classList={{ "rounded-md": props.rounded, "shadow-md": !props.flat }}
+    >
+      {safeChildren()}
     </div>
   );
 };
